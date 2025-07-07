680f417e2b8968e2a985b6f049d7cfcc3e4b6e0f
Gbebia2 <gbebia2@gmail.com>
2025-07-07 06:01:22 +0100

    clearer comment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,9 +8,9 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        //Create a new array of doubles with size equal to 'length'
-        //Loop from 0 up to length - 1 to fill each array position with a multiple of 'number'
-        //Return the filled array
+        // Declare a new array of doubles called results with length slots to store each multiple
+        // Use a for-loop to iterate from 0 up to (length - 1), calculating number × (i + 1) and assigning it to results[i]
+        // Return the completed array of calculated multiples
 
         double[] results = new double[length];
         for (int i = 0; i < length; i++)
@@ -30,9 +30,9 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // Use GetRange to copy the last 'amount' elements from the original list
-        // Insert the last 'amount' elements (copied using GetRange) at index 0 to move them to the front of the list
-        // Remove the original last 'amount' elements from the end of the list to avoid duplication
+        // Use GetRange to copy the last amount elements from the original list
+        // Insert the last amount elements copied using GetRange at index 0 to move them to the front of the list
+        // Remove the original last amount elements from the end of the list to avoid duplication
 
         List<int> slice = data.GetRange(data.Count - amount, amount);
         data.InsertRange(0, slice);
