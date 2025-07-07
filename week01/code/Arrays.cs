9953b969cb4817ac541816f7bbd30c4921ee28c6
Gbebia2 <gbebia2@gmail.com>
2025-07-07 05:54:35 +0100

    assignments

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,26 +8,34 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        //Create a new array of doubles with size equal to 'length'
+        //Loop from 0 up to length - 1 to fill each array position with a multiple of 'number'
+        //Return the filled array
 
-        return []; // replace this return statement with your own
+        double[] results = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            results[i] = number * (i + 1);
+        }
+
+        return results;
     }
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
     /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
-    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
+    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, ce2w  opi      6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Use GetRange to copy the last 'amount' elements from the original list
+        // Insert the last 'amount' elements (copied using GetRange) at index 0 to move them to the front of the list
+        // Remove the original last 'amount' elements from the end of the list to avoid duplication
+
+        List<int> slice = data.GetRange(data.Count - amount, amount);
+        data.InsertRange(0, slice);
+        data.RemoveRange(data.Count - amount, amount);
     }
 }
